c9be69e0f5e71cababe1c52e9a620b23d902a1a5
William Lam <wlam@vmware.com>
2020-02-21 10:22:34 -0800

    Pull Event Router Image 1683830 + updated Event Router to include stats deployment
    Signed-off-by: William Lam <wlam@vmware.com>

--- files/setup-05-event-processor.sh
+++ files/setup-05-event-processor.sh
@@ -20,36 +20,48 @@ if [ "${EVENT_PROCESSOR_TYPE}" == "AWS EventBridge" ]; then
     echo -e "\e[92mSetting up AWS Event Bridge Processor ..." > /dev/console
     cat > ${EVENT_ROUTER_CONFIG} << __AWS_EVENTBRIDGE_PROCESSOR__
 [{
-    "type": "stream",
-    "provider": "vmware_vcenter",
-    "address": "https://${VCENTER_SERVER}/sdk",
-    "auth": {
-        "method": "user_password",
-        "secret": {
-            "username": "${VCENTER_USERNAME}",
-            "password": "${VCENTER_PASSWORD}"
-        }
-    },
-    "options": {
-        "insecure": "${VCENTER_DISABLE_TLS}"
-    }
-},
-{
-    "type": "processor",
-    "provider": "aws_event_bridge",
-    "auth": {
-        "method": "access_key",
-        "secret": {
-            "aws_access_key_id": "${AWS_EVENTBRIDGE_ACCESS_KEY}",
-            "aws_secret_access_key": "${AWS_EVENTBRIDGE_ACCESS_SECRET}"
-        }
-    },
-    "options": {
-        "aws_region": "${AWS_EVENTBRIDGE_REGION}",
-        "aws_eventbridge_event_bus": "${AWS_EVENTBRIDGE_EVENT_BUS}",
-        "aws_eventbridge_rule_arn": "${AWS_EVENTBRIDGE_RULE_ARN}"
-    }
-}
+		"type": "stream",
+		"provider": "vmware_vcenter",
+		"address": "https://${VCENTER_SERVER}/sdk",
+		"auth": {
+			"method": "user_password",
+			"secret": {
+				"username": "${VCENTER_USERNAME}",
+				"password": "${VCENTER_PASSWORD}"
+			}
+		},
+		"options": {
+			"insecure": "${VCENTER_DISABLE_TLS}"
+		}
+	},
+	{
+		"type": "processor",
+		"provider": "aws_event_bridge",
+		"auth": {
+			"method": "access_key",
+			"secret": {
+				"aws_access_key_id": "${AWS_EVENTBRIDGE_ACCESS_KEY}",
+				"aws_secret_access_key": "${AWS_EVENTBRIDGE_ACCESS_SECRET}"
+			}
+		},
+		"options": {
+			"aws_region": "${AWS_EVENTBRIDGE_REGION}",
+			"aws_eventbridge_event_bus": "${AWS_EVENTBRIDGE_EVENT_BUS}",
+			"aws_eventbridge_rule_arn": "${AWS_EVENTBRIDGE_RULE_ARN}"
+		}
+	},
+	{
+		"type": "metrics",
+		"provider": "internal",
+		"address": "0.0.0.0:8080",
+		"auth": {
+			"method": "basic_auth",
+			"secret": {
+				"username": "admin",
+				"password": "${ROOT_PASSWORD}"
+			}
+		}
+	}
 ]
 __AWS_EVENTBRIDGE_PROCESSOR__
 else
@@ -66,35 +78,47 @@ else
 
     cat > ${EVENT_ROUTER_CONFIG} << __OPENFAAS_PROCESSOR__
 [{
-    "type": "stream",
-    "provider": "vmware_vcenter",
-    "address": "https://${VCENTER_SERVER}/sdk",
-    "auth": {
-        "method": "user_password",
-        "secret": {
-            "username": "${VCENTER_USERNAME}",
-            "password": "${VCENTER_PASSWORD}"
-        }
-    },
-    "options": {
-        "insecure": "${VCENTER_DISABLE_TLS}"
-    }
-},
-{
-    "type": "processor",
-    "provider": "openfaas",
-    "address": "http://gateway.openfaas:8080",
-    "auth": {
-        "method": "basic_auth",
-        "secret": {
-            "username": "admin",
-            "password": "${ROOT_PASSWORD}"
-        }
-    },
-    "options": {
-        "async": "false"
-    }
-}
+		"type": "stream",
+		"provider": "vmware_vcenter",
+		"address": "https://${VCENTER_SERVER}/sdk",
+		"auth": {
+			"method": "user_password",
+			"secret": {
+				"username": "${VCENTER_USERNAME}",
+				"password": "${VCENTER_PASSWORD}"
+			}
+		},
+		"options": {
+			"insecure": "${VCENTER_DISABLE_TLS}"
+		}
+	},
+	{
+		"type": "processor",
+		"provider": "openfaas",
+		"address": "http://gateway.openfaas:8080",
+		"auth": {
+			"method": "basic_auth",
+			"secret": {
+				"username": "admin",
+				"password": "${ROOT_PASSWORD}"
+			}
+		},
+		"options": {
+			"async": "false"
+		}
+	},
+	{
+		"type": "metrics",
+		"provider": "internal",
+		"address": "0.0.0.0:8080",
+		"auth": {
+			"method": "basic_auth",
+			"secret": {
+				"username": "admin",
+				"password": "${ROOT_PASSWORD}"
+			}
+		}
+	}
 ]
 __OPENFAAS_PROCESSOR__
 fi
