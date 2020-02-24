6c34f73e145c2d8c4a5e5913b4bc3f29258ce7b6
Michael Gasch <mgasch@vmware.com>
2020-02-24 22:38:52 +0100

    Update Python Examples for CloudEvents
    Signed-off-by: Michael Gasch <mgasch@vmware.com>

--- examples/python/esx-mtu-fixer/create_secret.sh
+++ examples/python/esx-mtu-fixer/create_secret.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+set -exo pipefail
+
 # Note: the default function namespace is openfaas-fn
 # you can change this namespace to your own
-kubectl create secret generic vc-credentials -n openfaas-fn \ 
-    --from-literal=vc-password='<vcenter_pass>' \
+kubectl -n openfaas-fn create secret generic vc-credentials --from-literal=vc-password='<vcenter_pass>' \
     --from-literal=vc-host='<vcenter_host_ip_fqdn>' \
     --from-literal=vc-user='<vcenter_username>'
