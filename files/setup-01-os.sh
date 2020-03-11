6b387821677c67d0f0bc343fb257b13d2b5b7d59
William Lam <wlam@vmware.com>
2020-03-11 05:29:37 -0700

    Support customization of Docker Bridge Network
    Signed-off-by: William Lam <wlam@vmware.com>
    
    Fixed default Docker Bridge Network Address
    
    Signed-off-by: William Lam <wlam@vmware.com>

--- files/setup-01-os.sh
+++ files/setup-01-os.sh
@@ -11,3 +11,13 @@ systemctl stop sshd
 
 echo -e "\e[92mConfiguring OS Root password ..." > /dev/console
 echo "root:${ROOT_PASSWORD}" | /usr/sbin/chpasswd
+
+if [ "${DOCKER_NETWORK_CIDR}" != "172.17.0.1/16" ]; then
+    echo -e "\e[92mConfiguring Docker Bridge Network ..." > /dev/console
+    cat > /etc/docker/daemon.json << EOF
+{
+    "bip": "${DOCKER_NETWORK_CIDR}"
+}
+EOF
+systemctl restart docker
+fi
