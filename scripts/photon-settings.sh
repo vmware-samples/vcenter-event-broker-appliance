4339b9e9ec052fd65ef64ad192cb1e6e63090bd9
William Lam <wlam@vmware.com>
2020-10-13 15:24:21 -0700

    Renaming version in BOM to represent Github Repo Tag
    Signed-off-by: William Lam <wlam@vmware.com>

--- scripts/photon-settings.sh
+++ scripts/photon-settings.sh
@@ -40,7 +40,7 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/GOOGLE-RPM-GPG-KEY
 EOF
-K8S_VERSION=$(jq -r < ${VEBA_BOM_FILE} '.["kubernetes"].version' | sed 's/v//g')
+K8S_VERSION=$(jq -r < ${VEBA_BOM_FILE} '.["kubernetes"].gitRepoTag' | sed 's/v//g')
 # Ensure kubelet is updated to the latest desired K8s version
 tdnf install -y kubelet-${K8S_VERSION} kubectl-${K8S_VERSION} kubeadm-${K8S_VERSION}
 
