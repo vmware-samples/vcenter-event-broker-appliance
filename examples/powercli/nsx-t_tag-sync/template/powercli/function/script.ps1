f467eda54f846016c14a719e7050520f86ed01bd
root <root@phxlvveba01.itplab.local>
2021-02-01 23:45:29 +0000

    Revert "Craig Straka <craig.straka@it-partners.com>"
    This reverts commit 3619034fb79be288bc60fcfecf8a3ce795d4921d.
    
    fixing git commits.

--- examples/powercli/nsx-t_tag-sync/template/powercli/function/script.ps1
+++
@@ -1,2 +0,0 @@
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore  -DisplayDeprecationWarnings $false -ParticipateInCeip $false -Scope AllUsers -Confirm:$false
-write-host "write your powercli code here"
