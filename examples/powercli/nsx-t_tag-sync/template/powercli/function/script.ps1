efa1193292c9d1418a6e4281b27e991c8cf64168
Craig Straka <craig.straka@it-partners.com>
2021-02-01 18:25:49 -0700

    Signed-off-by: craig straka <craig.straka@it-partners.com>

---
+++ examples/powercli/nsx-t_tag-sync/template/powercli/function/script.ps1
@@ -0,0 +1,2 @@
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore  -DisplayDeprecationWarnings $false -ParticipateInCeip $false -Scope AllUsers -Confirm:$false
+write-host "write your powercli code here"
