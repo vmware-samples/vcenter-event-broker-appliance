6ef86a31f3461b19053faea346772d01551f9e70
Craig Straka <craig.straka@it-partners.com>
2021-02-16 14:04:58 -0700

    vSphere tag synchronization to NSX-T
    Signed-off-by: Craig Straka <craig.straka@it-partners.com>

---
+++ examples/powershell/nsx-t_tag-sync-vro/template/powercli/function/script.ps1
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore  -DisplayDeprecationWarnings $false -ParticipateInCeip $false -Scope AllUsers -Confirm:$false
+write-host "write your powercli code here"
+=======
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore  -DisplayDeprecationWarnings $false -ParticipateInCeip $false -Scope AllUsers -Confirm:$false
+write-host "write your powercli code here"
+>>>>>>> fa117ff (Submitted powercli/nsx-t_tag-sync function.)
