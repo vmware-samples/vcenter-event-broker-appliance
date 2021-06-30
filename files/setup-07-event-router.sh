c8678cbc496a12b2e180da22c0177c314fdf413a
Robert Guske <rguske@vmware.com>
2021-06-30 20:50:20 +0200

    fix: Adjusted the kubectl wait timeout values to address issue 468
    Closes: #468
    Signed-off-by: Robert Guske <rguske@vmware.com>

--- files/setup-07-event-router.sh
+++ files/setup-07-event-router.sh
@@ -88,4 +88,4 @@ EOF
 
 kubectl apply -f /root/config/event-router-clusterrole.yaml
 kubectl -n vmware-system apply -f /root/config/event-router-k8s.yaml
-kubectl wait deployment --all --timeout=-1s --for=condition=Available -n vmware-system
+kubectl wait deployment --all --timeout=3m --for=condition=Available -n vmware-system
