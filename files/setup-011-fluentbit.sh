dd2f4ab89a9492ee08c4b78674bca63a38aac230
William Lam <wlam@vmware.com>
2021-09-07 12:53:30 -0700

    fix: Update Fluentbit config using YTT overlay
    Closes: #572
    Signed-off-by: William Lam <wlam@vmware.com>

--- files/setup-011-fluentbit.sh
+++ files/setup-011-fluentbit.sh
@@ -7,15 +7,21 @@
 set -euo pipefail
 
 VEBA_BOM_FILE=/root/config/veba-bom.json
+VEBA_CONFIG_FILE=/root/config/veba-config.json
 
 # Flutenbit Config files
-FLUENTBIT_TEMPLATE=/root/config/fluentbit/templates/fluentbit-ds-template.yaml
-FLUENTBIT_CONFIG=/root/config/fluentbit/fluentbit-ds.yaml
+FLUENTBIG_CONFIGMAP_OVERLAY=/root/download/fluentbit/overlay.yaml
+FLUENTBIT_CONFIGMAP_TEMPLATE=/root/config/fluentbit/templates/fluentbit-configmap-template.yaml
+FLUENTBIT_CONFIGMAP_CONFIG=/root/config/fluentbit/fluentbit-configmap.yaml
+
+FLUENTBIT_DEPLOYMENT_TEMPLATE=/root/config/fluentbit/templates/fluentbit-ds-template.yaml
+FLUENTBIT_DEPLOYMENT_CONFIG=/root/config/fluentbit/fluentbit-ds.yaml
 
 # Apply YTT overlay
-ytt --data-value-file bom=${VEBA_BOM_FILE} -f ${FLUENTBIT_TEMPLATE} > ${FLUENTBIT_CONFIG}
+ytt --data-value-file config=${VEBA_CONFIG_FILE} -f ${FLUENTBIG_CONFIGMAP_OVERLAY} -f ${FLUENTBIT_CONFIGMAP_TEMPLATE} > ${FLUENTBIT_CONFIGMAP_CONFIG}
+ytt --data-value-file bom=${VEBA_BOM_FILE} -f ${FLUENTBIT_DEPLOYMENT_TEMPLATE} > ${FLUENTBIT_DEPLOYMENT_CONFIG}
 
 kubectl apply -f /root/config/fluentbit/fluentbit-preperations.yaml
-kubectl apply -f /root/config/fluentbit/fluentbit-configmap.yaml
-kubectl apply -f ${FLUENTBIT_CONFIG}
+kubectl apply -f ${FLUENTBIT_CONFIGMAP_CONFIG}
+kubectl apply -f ${FLUENTBIT_DEPLOYMENT_CONFIG}
 kubectl wait --for=condition=ready pod -l k8s-app=fluent-bit --timeout=3m -n vmware-system
