e3e8e7ba9cd494158fcdb28fcb1e95b0b99cddda
William Lam <wlam@vmware.com>
2021-09-14 13:28:03 -0700

    feat: Add VMware Horizon Slack example function
    Closes: #588
    Signed-off-by: William Lam <wlam@vmware.com>

---
+++ examples/knative/powershell/kn-ps-horizon-login-slack/handler.ps1
@@ -0,0 +1,83 @@
+Function Process-Init {
+   [CmdletBinding()]
+   param()
+   Write-Host "$(Get-Date) - Processing Init`n"
+
+   Write-Host "$(Get-Date) - Init Processing Completed`n"
+}
+
+Function Process-Shutdown {
+   [CmdletBinding()]
+   param()
+   Write-Host "$(Get-Date) - Processing Shutdown`n"
+
+   Write-Host "$(Get-Date) - Shutdown Processing Completed`n"
+}
+
+Function Process-Handler {
+   [CmdletBinding()]
+   param(
+      [Parameter(Position=0,Mandatory=$true)][CloudNative.CloudEvents.CloudEvent]$CloudEvent
+   )
+
+   # Decode CloudEvent
+   try {
+      $cloudEventData = $cloudEvent | Read-CloudEventJsonData -Depth 10
+   } catch {
+      throw "`nPayload must be JSON encoded"
+   }
+
+   try {
+      $jsonSecrets = ${env:SLACK_SECRET} | ConvertFrom-Json
+   } catch {
+      throw "`nK8s secrets `$env:SLACK_SECRET does not look to be defined"
+   }
+
+   if(${env:FUNCTION_DEBUG} -eq "true") {
+      Write-Host "$(Get-Date) - DEBUG: K8s Secrets:`n${env:SLACK_SECRET}`n"
+
+      Write-Host "$(Get-Date) - DEBUG: CloudEventData`n $(${cloudEventData} | Out-String)`n"
+   }
+
+   # Strip "https://" from source
+   $horizonServer = ${cloudEvent}.source -replace "https://","" -replace "/",""
+
+   # Construct Slack message object
+   $payload = @{
+      attachments = @(
+         @{
+            pretext = ":rotating_light: Failed login detected from Horizon Server: ${horizonServer} :rotating_light:";
+            fields = @(
+               @{
+                     title = "Messsage";
+                     value = $cloudEventData.message
+                     short = "false";
+               }
+               @{
+                     title = "Time";
+                     value = $cloudEvent.time;
+                     short = "false";
+               }
+            )
+         }
+      )
+   }
+
+   # Convert Slack message object into JSON
+   $body = $payload | ConvertTo-Json -Depth 5
+
+   if(${env:FUNCTION_DEBUG} -eq "true") {
+      Write-Host "$(Get-Date) - DEBUG: `"$body`""
+   }
+
+   Write-Host "$(Get-Date) - Sending Webhook payload to Slack ..."
+   $ProgressPreference = "SilentlyContinue"
+
+   try {
+      Invoke-WebRequest -Uri $(${jsonSecrets}.SLACK_WEBHOOK_URL) -Method POST -ContentType "application/json" -Body $body
+   } catch {
+      throw "$(Get-Date) - Failed to send Slack Message: $($_)"
+   }
+
+   Write-Host "$(Get-Date) - Successfully sent Webhook ..."
+}
