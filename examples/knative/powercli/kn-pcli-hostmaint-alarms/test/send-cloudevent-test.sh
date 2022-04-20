e6ebd986c75f6e686e9dd2cbf21a4a62a42f3173
Patrick Kremer <pkremer@vmware.com>
2022-04-20 14:29:04 -0500

    feat: Port hostmaint alarms function to Knative
    Closes #856
    
    Signed-off-by: Patrick Kremer <pkremer@vmware.com>

---
+++ examples/knative/powercli/kn-pcli-hostmaint-alarms/test/send-cloudevent-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# The ce-subject value should match the event router subject in function.yaml
+echo "Testing Function ..."
+PAYLOAD_PATH="enter-maint-payload.json"
+SUBJECT="EnteredMaintenanceModeEvent"
+
+if [ $# -gt 0 ]; then
+    if test -f "$1"; then
+        PAYLOAD_PATH=$1
+    else
+        echo "$1 not found"
+        exit 1
+    fi
+
+    if [ $# -gt 1 ]; then
+        SUBJECT=$2
+    fi
+fi
+curl -d@$PAYLOAD_PATH \
+    -H "Content-Type: application/json" \
+    -H 'ce-specversion: 1.0' \
+    -H 'ce-id: d70079f9-fddd-4b7f-aa76-1193f28b0611' \
+    -H 'ce-source: https://vcenter.local/sdk' \
+    -H 'ce-type: com.vmware.event.router/event' \
+    -H 'ce-subject: '$SUBJECT \
+    -X POST localhost:8080
+
+echo "See docker container console for output"
